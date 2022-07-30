0d6dbb7cacb1ed4f98e736e52c27487d0679862b
Soumyadip Karmakar <shawamar011@gmail.com>
2022-07-31 00:32:18 +0530

    name inputed

--- src/components/Qrcode.js
+++ src/components/Qrcode.js
@@ -1,12 +1,13 @@
 import React from "react";
-import {useState,useRef} from "react";
-import {QRCodeCanvas} from "qrcode.react"
+import { useState, useRef } from "react";
+import { QRCodeCanvas } from "qrcode.react"
 import "./qrcode.css";
 
 
 const Qrcode = () => {
-const [url,setUrl] = useState("")
-const qrRef= useRef()
+    const [url, setUrl] = useState("")
+    const [name, setName] = useState("")
+    const qrRef = useRef()
 
     const downloadQRCode = (e) => {
         e.preventDefault();
@@ -14,7 +15,7 @@ const qrRef= useRef()
         let image = canvas.toDataURL("image/png");
         let anchor = document.createElement("a");
         anchor.href = image;
-        anchor.download = `qr-code.png`;
+        anchor.download = `${name}.png`;
         document.body.appendChild(anchor);
         anchor.click();
         document.body.removeChild(anchor);
@@ -23,38 +24,44 @@ const qrRef= useRef()
 
     const qrCodeEncoder = (e) => {
         setUrl(e.target.value)
-        
+
+    }
+
+    const setNameEncoder = (e) => {
+        setName(e.target.value)
     }
 
 
     const qrcode = (
-    <QRCodeCanvas
-      id="qrCode"
-      value={url}
-      size={300}
-      bgColor={"#fff"}
-      level={"H"}
-    />
+        <QRCodeCanvas
+            id="qrCode"
+            value={url}
+            size={300}
+            bgColor={"#fff"}
+            level={"H"}
+        />
     )
 
-    return(
-    <div>
-        <div className="qrcode_container">
-            <div className="qr_code_img">
-                <div ref={qrRef}>{qrcode}</div>
+    return (
+        <div>
+            <div className="qrcode_container">
+                <div className="qr_code_img">
+                    <div ref={qrRef}>{qrcode}</div>
 
-            </div>
-            <div className="qr_code_input_group">
-                <form className="qr_code_input_group" onSubmit={downloadQRCode}>
-                    <div className="qr_code_input_group_container">
+                </div>
+                <div className="qr_code_input_group">
+                    <form className="qr_code_input_group" onSubmit={downloadQRCode}>
+                        <div className="qr_code_input_group_container">
+                            <div><label className="qr_code_label">Enter Name</label></div>
+                            <div><input className="qr_code_label_input" type="text" value={name} onChange={setNameEncoder} placeholder="Please enter your Name " /></div>
                             <div><label className="qr_code_label">Enter URL</label></div>
-                          <div><input className="qr_code_label_input" type="text" value={url} onChange={qrCodeEncoder} placeholder="Please enter your URL "/></div>
-                          <div><button type="submit" className="qr_code_label_button" disabled={!url} >Download QR Code</button></div>
-                    </div>  
-                </form>
+                            <div><input className="qr_code_label_input" type="text" value={url} onChange={qrCodeEncoder} placeholder="Please enter your URL " /></div>
+                            <div><button type="submit" className="qr_code_label_button" disabled={!url} >Download QR Code</button></div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>       
     )
 }
 
